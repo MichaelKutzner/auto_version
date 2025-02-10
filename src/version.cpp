e3a680d27b466a4927e7c5ed84a2be837a5a2652
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-02-10 09:19:37 +0100

    Add dynamic version

--- src/version.cpp
+++ src/version.cpp
@@ -1,7 +1,15 @@
 #include "version.h"
 
 #if __has_include("./version_dynamic.cpp.in")
+
+#if defined(AUTO_BUILD_VERSION)
+constexpr auto version_details = std::string_view{AUTO_BUILD_VERSION};
+#else
+constexpr auto version_details = std::string_view{"unknown"};
+#endif
+
 #include "./version_dynamic.cpp.in"
+
 #else
 #include "./version_static.cpp.in"
 #endif
